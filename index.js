300031c8cecea06303a856afbd635045fcd70bf0
Veniamin <sven1980@list.ru>
2020-06-21 15:38:03 +0300

    feat: todolist done

--- index.js
+++ index.js
@@ -10,22 +10,73 @@ const todos = [
 
 const todosListElement = document.querySelector('.todos__list');
 const todosFormElement = document.querySelector('.todos__form');
+const todosSubmitBtn = todosFormElement.querySelector('.todos__submit-btn');
 const todosInputElement = document.querySelector('.todos__input');
-const todosTemplateElement =  document.querySelector('.todo-template'); 
+const todosTemplateElement =  document.querySelector('.todo-template');
+
+let editedElement = null;
+
+function addTodoListeners(todo) {
+  todo.querySelector('.todo__btn_type_delete').addEventListener('click', handleDelete);
+  todo.querySelector('.todo__btn_type_edit').addEventListener('click', handleEdit);
+  todo.querySelector('.todo__btn_type_duplicate').addEventListener('click', handleDuplicate);
+}
+
+function handleDelete(evt) {
+  const todo = evt.currentTarget.parentNode;
+  todo.remove();
+}
+
+function setTodoToForm(todo) {
+  const text = todo.querySelector('.todo__text').textContent;
+  todosInputElement.value = text;
+  todosSubmitBtn.textContent = 'Сохранить';
+}
+
+function resetTodoForm() {
+  editedElement = null;
+  todosInputElement.value = '';
+  todosSubmitBtn.textContent = 'Добавить';
+}
+
+function handleEdit(evt) {
+  const todo = evt.currentTarget.parentNode;
+  
+  editedElement = todo;
+  setTodoToForm(todo);
+}
+
+function handleDuplicate(evt) {
+  const todo = evt.currentTarget.parentNode;
+  const duplicate = todo.cloneNode(true);
+
+  addTodoListeners(duplicate);
+  todo.after(duplicate);
+}
 
 function addTodo(text) {
   const todo = todosTemplateElement.content.cloneNode(true);
 
   todo.querySelector('.todo__text').textContent = text;
+  addTodoListeners(todo);
 
   todosListElement.prepend(todo);
 }
 
-todos.forEach(addTodo);
-
-todosFormElement.addEventListener('submit', evt => {
+function handleFormSubmit (evt) {
   evt.preventDefault();
 
   const text = todosInputElement.value;
-  addTodo(text);
-});
+
+  if (editedElement) {
+    editedElement.querySelector('.todo__text').textContent = text;
+  } else {
+    addTodo(text);
+  }
+
+  resetTodoForm();
+}
+
+todos.forEach(addTodo);
+
+todosFormElement.addEventListener('submit', handleFormSubmit);
