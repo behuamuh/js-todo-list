442ad3c31907997c6d6eac1f89b3e8e160f0b8bd
Veniamin Shitikov <sven1980@list.ru>
2022-05-30 19:57:41 +0300

    feat: added editing

--- index.js
+++ index.js
@@ -12,20 +12,40 @@ const todoListElement = document.querySelector('.todos__list');
 const todoFormElement = document.querySelector('.todos__form');
 const todoInputElement = todoFormElement.querySelector('.todos__input');
 const todoTemplateElement = document.querySelector('.todo-template');
+const todoSubmitButtonElement = document.querySelector('.todos__submit-btn');
+
 const getTodoByEvent = e => e.currentTarget.closest('.todo');
+const getTodoTextElement = todo => todo.querySelector('.todo__text');
+
+let editedTodo = null;
+
+const deleteTodo = e => {
+  const todo = getTodoByEvent(e);
+
+  todo.remove();
+};
+
+const editTodo = e => {
+  const todo = getTodoByEvent(e);
+
+  editedTodo = todo;
+
+  todoInputElement.value = getTodoTextElement(editedTodo).textContent;
+  todoSubmitButtonElement.textContent = 'Сохранить';
+};
+
+const addTodoListeners = todo => {
+  todo.querySelector('.todo__btn_type_delete').addEventListener('click', deleteTodo);
+  todo.querySelector('.todo__btn_type_edit').addEventListener('click', editTodo);
+};
 
 const createTodo = text => {
   const todo = todoTemplateElement.content
     .querySelector('.todo')
     .cloneNode(true);
 
-  todo.querySelector('.todo__text').textContent = text;
-
-  todo.querySelector('.todo__btn_type_delete').addEventListener('click', e => {
-    const todo = getTodoByEvent(e);
-
-    todo.remove();
-  });
+  getTodoTextElement(todo).textContent = text;
+  addTodoListeners(todo);
 
   return todo;
 };
@@ -41,7 +61,14 @@ const handleTodoSubmit = e => {
 
   const text = todoInputElement.value;
 
-  addTodo(text);
+  if (editedTodo) {
+    getTodoTextElement(editedTodo).textContent = text;
+    todoSubmitButtonElement.textContent = 'Добавить';
+
+    editedTodo = null;
+  } else {
+    addTodo(text);
+  }
 
   todoFormElement.reset();
 };
